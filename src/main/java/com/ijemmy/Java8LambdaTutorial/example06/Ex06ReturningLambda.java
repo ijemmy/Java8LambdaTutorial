344044d4f11e81046c341f4cc73d6a09059bc5ca
ijemmy <ijemmy@users.noreply.github.com>
2014-08-10 18:09:46 -0400

    Add example 6, returning Lambda (Consumer)

---
+++ src/main/java/com/ijemmy/Java8LambdaTutorial/example06/Ex06ReturningLambda.java
@@ -0,0 +1,23 @@
+package com.ijemmy.Java8LambdaTutorial.example06;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Ex06ReturningLambda {
+	
+	public static void main(String[] args) {
+		List<Integer> l = Arrays.asList(1, 3, 2, 4);
+		l.forEach(getConsumer());
+		
+	}
+	
+	public static Consumer<Integer> getConsumer(){
+		int fixedNumber = 10;
+		//fixedNumber++; //Must be effectively final
+		return x -> {
+			System.out.println(x * fixedNumber);
+		};
+	}
+	
+}
